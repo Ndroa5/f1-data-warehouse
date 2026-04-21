ddc8b6c2afdd9387b9c46efe7c1d76474df1db28
Andrej Vukoje <andrej.vukoje@vega.local>
2026-04-21 11:07:07 +0200

    Add run() function to bronze, silver and gold main.py for Airflow integration

--- gold/main.py
+++ gold/main.py
@@ -4,7 +4,16 @@ from load import load_to_gold
 
 engine = create_engine('postgresql://postgres:postgres123@localhost:5432/f1_warehouse')
 
-if __name__ == "__main__":
+def create_tables():
     Base.metadata.create_all(engine)
     print("✅ Gold tabele kreirane!")
+
+def load():
     load_to_gold(engine)
+
+def run():
+    create_tables()
+    load()
+
+if __name__ == "__main__":
+    run()
